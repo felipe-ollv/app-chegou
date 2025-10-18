b87ca418c6d70e34f49920bcf1cb4c39c7cb9559
Felipe Oliveira <felipeoliveira264@gmail.com>
2025-10-18 12:35:28 -0300

    ajuste login

--- src/app/(tabs)/showcase/page.tsx
+++ src/app/(tabs)/showcase/page.tsx
@@ -42,7 +42,6 @@ export default function ShowcaseScreen() {
       fetchPackageList();
     }, [])
   );
-
   useEffect(() => {
     if (!expoPushToken) return;
 
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, TextInput, TouchableOpacity, Alert } from "react-native";
+import { View } from "react-native";
 import { useRouter } from "expo-router";
 import api, { setToken } from '../interceptor/axios-config';
 import { styles } from "../styles/index-styles";
-import HeaderComponent from "../components/header/component";
 import LoadingComponent from '../components/loading/component';
 import ToastComponent from '../components/toast/component';
-import formatPhoneNumber from "../utils/formatPhoneNumber";
 import ActionStorage from "./(auth)/midleware/authStorage";
 import { jwtDecode } from "jwt-decode";
 import { useUser } from "../context/user.context";
-import colors from "../../colors-app/colors";
 import { checkDeviceSupport } from "./service/check-identity-capability";
 import * as LocalAuthentication from 'expo-local-authentication';
-
 import SigninScreen from "./(auth)/signin/page";
 
 export default function InitScreen() {
@@ -23,16 +19,15 @@ export default function InitScreen() {
 
   useEffect(() => {
     handleBiometricLogin();
-  }, [])
+  }, []);
 
   const handleBiometricLogin = async () => {
     const supported = await checkDeviceSupport();
 
     if (!supported) {
       setLoading(false);
-      router.replace('./(auth)/signin/page');
       return;
-    }  
+    }
 
     const result = await LocalAuthentication.authenticateAsync({
       promptMessage: 'Autentique-se para entrar',
@@ -43,29 +38,31 @@ export default function InitScreen() {
     if (result.success) {
       handleExistToken();
     } else {
-      ToastComponent({ type: 'warning', text1: 'Atenção!', text2: 'A autenticação falhou!'});
-      setLoading(false)
+      ToastComponent({
+        type: 'warning',
+        text1: 'Atenção!',
+        text2: 'A autenticação foi cancelada ou falhou!',
+      });
+      setLoading(false);
     }
-  }
+  };
 
   const handleExistToken = async () => {
-    setLoading(true);
     ActionStorage.getToken().then(token => {
       if (token) {
         setToken(token);
         const decoded = jwtDecode(token);
         setUserData(decoded);
         router.replace("/showcase/page");
-        setLoading(false);
       } else {
         setLoading(false);
       }
     });
-  }
+  };
 
   return (
     <View style={styles.container}>
-      {!loading ? (
+      {loading ? (
         <LoadingComponent />
       ) : (
         <SigninScreen />
