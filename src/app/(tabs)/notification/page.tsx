3a9e9b92eb55c52cf866dfe6c0188ec5249de8f3
Felipe Oliveira <felipeoliveira264@gmail.com>
2026-01-04 19:24:41 -0300

    carregar avisos com a page no foco

--- src/app/(tabs)/notification/page.tsx
+++ src/app/(tabs)/notification/page.tsx
@@ -4,10 +4,11 @@ import { Pressable, Text, View, ScrollView } from "react-native";
 import { notificationStyles } from "../../../styles/notification-styles";
 import UploadPdfComponent from "../../../components/pdf/pdf-document-note";
 import { useUser } from "../../../context/user.context";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import ToastComponent from "../../../components/toast/component";
 import api from "../../../interceptor/axios-config";
 import ModalPdfView from "../../../components/modals/modal-view-pdf";
+import { useFocusEffect } from "expo-router";
 
 export default function NotificationScreen() {
   const [visible, setVisible] = useState(false);
@@ -34,6 +35,12 @@ export default function NotificationScreen() {
       });
     }
   };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchNoteCondominium();
+    }, [])
+  )
 
   const getPdfBase64 = async (path: string) => {
     try {
